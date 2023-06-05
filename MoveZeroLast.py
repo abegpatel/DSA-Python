b769d3bcf11696dd52f7030b28ebeed3f151846f
abegpatel <abegpatel@gmail.com>
2023-06-05 18:47:44 +0530

    DSA Python Mock Test-1

---
+++ MoveZeroLast.py
@@ -0,0 +1,1 @@
+def MoveZero(nums):    count = 0     for i in range(len(nums)):        if nums[i] != 0:            nums[count] = nums[i]            count+=1    while count < len(nums):        nums[count] = 0        count += 1    return numsnums=[0,1,0,3,12]print(MoveZero(nums))#Time Complexity - O(N) n is the size of the element#Space Complexity - O(1)
